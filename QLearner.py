787ee816e37deb566aa71965375cad2466dd52a0
Colten Appleby <colten.appleby@gmail.com>
2023-11-08 17:42:59 -0500

    created so sorry game + added Qlearner

---
+++ QLearner.py
@@ -0,0 +1,128 @@
+"""
+QLearner
+  		  	   		  		 		  		  		    	 		 		   		 		  
+Engineer: Colten Appleby
+"""
+
+import numpy as np
+class QLearner(object):
+    """  		  	   		  		 		  		  		    	 		 		   		 		  
+    This is a Q learner object.  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+    :param num_states: The number of states to consider.  		  	   		  		 		  		  		    	 		 		   		 		  
+    :type num_states: int  		  	   		  		 		  		  		    	 		 		   		 		  
+    :param num_actions: The number of actions available..  		  	   		  		 		  		  		    	 		 		   		 		  
+    :type num_actions: int  		  	   		  		 		  		  		    	 		 		   		 		  
+    :param alpha: The learning rate used in the update rule. Should range between 0.0 and 1.0 with 0.2 as a typical value.  		  	   		  		 		  		  		    	 		 		   		 		  
+    :type alpha: float  		  	   		  		 		  		  		    	 		 		   		 		  
+    :param gamma: The discount rate used in the update rule. Should range between 0.0 and 1.0 with 0.9 as a typical value.  		  	   		  		 		  		  		    	 		 		   		 		  
+    :type gamma: float  		  	   		  		 		  		  		    	 		 		   		 		  
+    :param rar: Random action rate: the probability of selecting a random action at each step. Should range between 0.0 (no random actions) to 1.0 (always random action) with 0.5 as a typical value.  		  	   		  		 		  		  		    	 		 		   		 		  
+    :type rar: float  		  	   		  		 		  		  		    	 		 		   		 		  
+    :param radr: Random action decay rate, after each update, rar = rar * radr. Ranges between 0.0 (immediate decay to 0) and 1.0 (no decay). Typically 0.99.  		  	   		  		 		  		  		    	 		 		   		 		  
+    :type radr: float  		  	   		  		 		  		  		    	 		 		   		 		  
+    :param dyna: The number of dyna updates for each regular update. When Dyna is used, 200 is a typical value.  		  	   		  		 		  		  		    	 		 		   		 		  
+    :type dyna: int  		  	   		  		 		  		  		    	 		 		   		 		  
+    :param verbose: If “verbose” is True, your code can print out information for debugging.  		  	   		  		 		  		  		    	 		 		   		 		  
+    :type verbose: bool  		  	   		  		 		  		  		    	 		 		   		 		  
+    """
+    def __init__(
+        self,
+        num_states=100,
+        num_actions=4,
+        alpha=0.2,
+        gamma=0.9,
+        rar=0.5,
+        radr=0.99,
+        dyna=0,
+        verbose=False,
+    ):
+        """
+        Constructor method
+        """
+        self.a = None
+        self.s = None
+        self.verbose = verbose
+        self.num_actions = num_actions
+        self.num_states = num_states
+        self.alpha = alpha
+        self.gamma = gamma
+        self.rar = rar
+        self.radr = radr
+        self.dyna = dyna
+        self.Q = np.zeros(shape=(num_states, num_actions))
+        self.T = np.full(shape=(num_states, num_actions, num_states), fill_value=0.0001)
+        self.R = np.zeros(shape=(num_states, num_actions))
+        self.moves = []
+
+    def author(self):
+        return "cappleby3"
+
+    def querysetstate(self, s):
+        """  		  	   		  		 		  		  		    	 		 		   		 		  
+        Update the state without updating the Q-table  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+        :param s: The new state  		  	   		  		 		  		  		    	 		 		   		 		  
+        :type s: int  		  	   		  		 		  		  		    	 		 		   		 		  
+        :return: The selected action  		  	   		  		 		  		  		    	 		 		   		 		  
+        :rtype: int  		  	   		  		 		  		  		    	 		 		   		 		  
+        """
+        self.s = s
+        self.a = np.random.randint(0, self.num_actions - 1)
+        # self.a = self.get_action(s)
+        if self.verbose: print(f"s = {s}, a = {self.a}")
+        return self.a
+
+    def get_action(self, state):
+        if np.random.rand() <= self.rar:
+            return np.random.randint(0, self.num_actions - 1)
+        else:
+            return np.argmax(self.Q[state])
+
+    def update_Q(self, state, action, s_prime, reward):
+        self.Q[state, action] = ((1-self.alpha) * self.Q[state, action]
+                                 + self.alpha * (reward + self.gamma * np.max(self.Q[s_prime])))
+
+    def hallucinate(self):
+        for i in range(0, self.dyna):
+            state, action, s_prime, reward = self.moves[np.random.randint(0, len(self.moves))]
+            self.update_Q(state, action, s_prime, reward)
+
+    def query(self, s_prime, r):
+        """  		  	   		  		 		  		  		    	 		 		   		 		  
+        Update the Q table and return an action  		  	   		  		 		  		  		    	 		 		   		 		  
+  		  	   		  		 		  		  		    	 		 		   		 		  
+        :param s_prime: The new state  		  	   		  		 		  		  		    	 		 		   		 		  
+        :type s_prime: int  		  	   		  		 		  		  		    	 		 		   		 		  
+        :param r: The immediate reward  		  	   		  		 		  		  		    	 		 		   		 		  
+        :type r: float  		  	   		  		 		  		  		    	 		 		   		 		  
+        :return: The selected action  		  	   		  		 		  		  		    	 		 		   		 		  
+        :rtype: int  		  	   		  		 		  		  		    	 		 		   		 		  
+        """
+
+        """      
+        1. We are first going to update the model. Hey this is what happened last turn
+        - The model knows the previous location and the last action (saved above)
+        - Now it was given the new location, and the reward
+        --> Update the model! with the previous move we gained this reward
+        
+        2. Now we need to generate a new action, and update our saved information
+        """
+
+        self.update_Q(self.s, self.a, s_prime, r)
+
+        self.moves.append([self.s, self.a, s_prime, r])
+        self.hallucinate()
+
+        self.a = self.get_action(s_prime)
+        self.s = s_prime
+        self.rar *= self.radr
+
+        if self.verbose:
+            print(f"s = {s_prime}, a = {self.a}, r={r}")
+
+        return self.a
+
+
+if __name__ == "__main__":
+    print("Remember Q from Star Trek? Well, this isn't him")
---
+++ so-sorry.py
@@ -0,0 +1,191 @@
+import random
+
+
+class SoSorry:
+
+    def __init__(self,
+        players,
+        player_name = 'player1',
+        player_position = 0,
+        verbose=False,
+    ):
+        self.number_of_players = players
+        self.player_name = player_name
+        self.curr_dealer = 0
+        self.curr_player = player_position
+        self.players = [f"Player {i}" for i in range(self.number_of_players)]
+        self.player_cards = [0 for _ in range(self.number_of_players)]
+        self.deck = [i for i in range(1, 14) for _ in range(4)]
+        self.verbose = verbose
+        self.traded = False
+
+    def game(self):
+        print("Welcome to So Sorry!")
+        print("The rules are simple:")
+        print("1. Each player is dealt a card")
+        print("2. The dealer will ask each player if they want to trade their card with the player to their left")
+        print("3. The dealer can draw a card")
+        print("4. The player with the lowest card is out of the game")
+        print("5. The dealer is now the player to the left of the player who was just eliminated")
+        print("6. Repeat steps 1-5 until there is only one player left")
+        print()
+        print("Let's play!")
+        print()
+        print("You are", self.players[self.curr_player])
+        if self.verbose: print('Game start!')
+
+        self.shuffle()
+
+        while len([x for x in self.player_cards if x > -1]) > 1:
+
+            self.deal_hand()
+
+            k = self.curr_dealer + 1
+            while True:
+                if k == self.number_of_players:
+                    k = 0
+                    continue
+                elif self.player_cards[k] == -1:
+                    k+=1
+                    continue
+                elif k == self.curr_player:
+                    self.players_turn()
+                    if self.curr_player == self.curr_dealer:
+                        break
+                    k += 1
+                    continue
+                elif k == self.curr_dealer:
+                    self.should_I_stay_or_should_I_go(k)
+                    break
+                self.should_I_stay_or_should_I_go(k)
+                k += 1
+
+            print("Everyone show their cards:", self.player_cards)
+
+            self.decide_losers()
+            self.update_dealer()
+            if self.verbose: print("Dealer is now", self.players[self.curr_dealer])
+
+            if len(self.deck) < len([x for x in self.player_cards if x > -1]):
+                self.shuffle()
+            print("------------------------------ NEXT ROUND ------------------------------")
+
+        print(f"Winner is {self.players[self.player_cards.index(max(self.player_cards))]}")
+
+    def decide_losers(self):
+        lowest_card = min(num for num in self.player_cards if num != -1)
+        losers = [i for i, x in enumerate(self.player_cards) if x == lowest_card]
+        for loser in losers:
+            if self.verbose: print(self.players[loser], f"had the lowest card [{lowest_card}] and is out of the game")
+            self.player_cards[loser] = -1
+
+    def update_dealer(self):
+        self.curr_dealer += 1
+        if self.curr_dealer == self.number_of_players:
+            self.curr_dealer = 0
+        if self.player_cards[self.curr_dealer] == -1:
+            self.update_dealer()
+
+    def should_I_stay_or_should_I_go(self, current_player):
+        if self.traded == True and self.player_cards[current_player] > self.player_cards[self.get_card_traded_index(current_player)]:
+            if self.verbose: print(f"{self.players[current_player]} is staying")
+            self.traded = False
+        # if random.random() > 0.5:
+        elif self.player_cards[current_player] < 7:
+            self.trade(current_player)
+        else:
+            if self.verbose: print(f"{self.players[current_player]} is staying")
+            self.traded = False
+
+    def player_to_trade_with(self, index):
+        if index == self.number_of_players:
+            return self.player_to_trade_with(0)
+        elif self.player_cards[index] == -1:
+            return self.player_to_trade_with(index+1)
+        return index
+
+    def trade(self, curr_index):
+        if curr_index == self.curr_dealer:
+            self.deal(curr_index)
+            if self.verbose: print(f"Dealer drew a", self.player_cards[curr_index])
+        else:
+            player_to_trade_with = self.player_to_trade_with(curr_index+1)
+            if self.verbose: print(f"{self.players[curr_index]} is trading with {self.players[player_to_trade_with]}")
+            self.player_cards[curr_index], self.player_cards[player_to_trade_with] = self.player_cards[player_to_trade_with], self.player_cards[curr_index]
+        self.traded = True
+
+    def players_turn(self):
+        print()
+        print("Your turn!")
+        print(f"Your card is a [{self.print_card(self.curr_player)}]")
+        if self.traded: print(f"You were forced to trade a [{self.print_card(self.get_card_traded_index(self.curr_player))}]")
+        print("What would you like to do?")
+        if self.curr_player == self.curr_dealer: print("1. Draw a card")
+        else: print("1. Trade")
+        print("2. Stay")
+        # print("3. Quit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            self.trade(self.curr_player)
+            print(f"Your card is now as [{self.print_card(self.curr_player)}]")
+        elif choice == "2":
+            # self.stay()
+            pass
+        elif choice == "3":
+            # self.quit()
+            pass
+        else:
+            print("Invalid choice. Try again.")
+            self.players_turn()
+        print()
+        print()
+
+    def get_card_traded_index(self, index):
+        if self.player_cards[index-1] == -1:
+            return self.get_card_traded_index(index-1)
+        else:
+            return index-1
+
+
+    def deal_hand(self):
+        if self.verbose: print(self.players[self.curr_dealer], "is dealing...")
+        self.traded = False
+
+        i = self.curr_dealer + 1
+        while i != self.curr_dealer:
+            if i == self.number_of_players:
+                i = 0
+                continue
+            if self.player_cards[i] == -1:
+                i += 1
+                continue
+            else:
+                self.deal(i)
+                i+=1
+        self.deal(self.curr_dealer)
+
+    def deal(self, player_index):
+        self.player_cards[player_index] = self.deck.pop()
+        pass
+
+    def shuffle(self):
+        if self.verbose: print("Shuffling deck...")
+        new_deck = [i for i in range(1, 13) for _ in range(4)]
+        self.deck = random.sample(new_deck, len(new_deck))
+
+    def print_card(self, player_index):
+        card_index = self.player_cards[player_index]
+        if card_index == 1:
+            return "Ace"
+        if card_index <= 10:
+            return card_index
+        if card_index == 11:
+            return "Jack"
+        if card_index == 12:
+            return "Queen"
+        if card_index == 13:
+            return "King"
+
+if __name__ == "__main__":
+    game = SoSorry(4, verbose=True)
+    game.game()
